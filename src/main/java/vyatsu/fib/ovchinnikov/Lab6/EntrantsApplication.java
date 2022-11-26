a61b61eba170111d7cb3153f6e943bb38a72bd9d
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2022-11-26 12:34:48 +0300

    Инициализация проекта.

---
+++ src/main/java/vyatsu/fib/ovchinnikov/Lab6/EntrantsApplication.java
@@ -0,0 +1,13 @@
+package vyatsu.fib.ovchinnikov.Lab6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vyatsu/fib/ovchinnikov/Lab6/EntrantsApplicationTests.java
@@ -0,0 +1,13 @@
+package vyatsu.fib.ovchinnikov.Lab6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
