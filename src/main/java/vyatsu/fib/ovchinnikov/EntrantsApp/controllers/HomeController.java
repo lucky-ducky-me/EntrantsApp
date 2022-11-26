7933c1f29a0ecf835b7919af239a7a8d3e97fa3e
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2022-11-26 16:37:32 +0300

    Изменены атрибуты домашнего контроллера.

--- src/main/java/vyatsu/fib/ovchinnikov/EntrantsApp/controllers/HomeController.java
+++ src/main/java/vyatsu/fib/ovchinnikov/EntrantsApp/controllers/HomeController.java
@@ -3,10 +3,8 @@ package vyatsu.fib.ovchinnikov.EntrantsApp.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Controller
-@EnableWebMvc
 public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
