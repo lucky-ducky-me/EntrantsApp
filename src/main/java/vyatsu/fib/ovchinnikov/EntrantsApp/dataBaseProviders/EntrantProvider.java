d80b3a70a3033ef386bb1b91cf1d8a420d96ee1d
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2022-11-26 19:56:16 +0300

    Добавлен провайдер базы данных для абитурентов. (Имитация).

---
+++ src/main/java/vyatsu/fib/ovchinnikov/EntrantsApp/dataBaseProviders/EntrantProvider.java
@@ -0,0 +1,73 @@
+package vyatsu.fib.ovchinnikov.EntrantsApp.dataBaseProviders;
+
+import vyatsu.fib.ovchinnikov.EntrantsApp.models.Entrant;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Провайдер для таблицы в абитуриентами.
+ * (Имитация).
+ */
+public class EntrantProvider implements IDataBaseProvider<Entrant> {
+
+    /**
+     * Список абитуриентов.
+     */
+    private ArrayList<Entrant> entrants = new ArrayList<>();
+
+    /**
+     * Получение абитуриента.
+     * @param id id.
+     * @return абитуриент.
+     */
+    @Override
+    public Optional<Entrant> get(UUID id) {
+        return entrants.stream()
+                .filter(entrant -> entrant.getId().equals(id))
+                .findAny();
+    }
+
+    /**
+     * Получение всех абитуриентов.
+     * @return список абитуриентов.
+     */
+    @Override
+    public List<Entrant> getAll() {
+        return new ArrayList<>(entrants);
+    }
+
+    /**
+     * Сохранение абитуриента в базу данных.
+     * @param object удаляемый абитуриент.
+     * @return успех сохранения.
+     */
+    @Override
+    public boolean save(Entrant object) {
+        if (get(object.getId()).isPresent()) {
+             throw new RuntimeException("Абитуриент с id: " + object.getId() + " уже существует.");
+        }
+
+        entrants.add(object);
+
+        return entrants.contains(object);
+    }
+
+    /**
+     * Удаление абитуриента из базы данных.
+     * @param object удаляемый абитуриент.
+     * @return успех удаления.
+     */
+    @Override
+    public boolean delete(Entrant object) {
+        if (get(object.getId()).isEmpty()) {
+            throw new RuntimeException("Абитуриент с id: " + object.getId() + " не существует.");
+        }
+
+        entrants.remove(object);
+
+        return !entrants.contains(object);
+    }
+}
