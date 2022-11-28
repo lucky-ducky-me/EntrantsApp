281c81e7268a3e582310b1d450f79e8b06b0cd74
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2022-11-28 18:32:20 +0300

    Класс для хранения количества величин.

---
+++ src/main/java/vyatsu/fib/ovchinnikov/EntrantsApp/models/Score.java
@@ -0,0 +1,17 @@
+package vyatsu.fib.ovchinnikov.EntrantsApp.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Счёт.
+ */
+public class Score {
+
+    /**
+     * Целочисленный счёт.
+     */
+    @Getter
+    @Setter
+    private Integer scoreInt;
+}
