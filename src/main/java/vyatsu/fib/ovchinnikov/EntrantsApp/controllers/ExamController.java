1d32ab14b9b885e133a6c2ec9f2a4d2d5073d3ac
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2022-11-28 19:29:40 +0300

    Добавлен метод в контроллере и отображение для добавления экзамена.

--- src/main/java/vyatsu/fib/ovchinnikov/EntrantsApp/controllers/ExamController.java
+++ src/main/java/vyatsu/fib/ovchinnikov/EntrantsApp/controllers/ExamController.java
@@ -1,11 +1,19 @@
 package vyatsu.fib.ovchinnikov.EntrantsApp.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 import vyatsu.fib.ovchinnikov.EntrantsApp.dataBaseProviders.EntrantExamMapProvider;
 import vyatsu.fib.ovchinnikov.EntrantsApp.dataBaseProviders.EntrantProvider;
 import vyatsu.fib.ovchinnikov.EntrantsApp.dataBaseProviders.ExamProvider;
+import vyatsu.fib.ovchinnikov.EntrantsApp.models.Exam;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  * Контроллер для экзаменов.
@@ -44,5 +52,37 @@ public class ExamController {
         return "exams";
     }
 
+    @GetMapping("/addExam")
+    public String addingExam(Model model) {
+        model.addAttribute("title", "Добавление экзамена.");
 
+        var exam = new Exam();
+
+        model.addAttribute("exam", exam);
+
+        return "addExam";
+    }
+
+    @PostMapping("/addExam")
+    public String addingExam(@ModelAttribute Exam exam
+            , Model model) {
+        try {
+            if (examProvider.getBySubject(exam.getSubject()).isPresent()) {
+                throw new Exception("Экзамен с таким именем уже существует.");
+            }
+
+            var savingSuccess = examProvider.save(exam);
+
+            if (!savingSuccess) {
+                throw new Exception("Возникли ошибки при добавлении экзамена в базу данных.");
+            }
+        }
+        catch (Exception ex) {
+            System.out.println(Arrays.toString(ex.getStackTrace()));
+
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+
+        return "redirect:/exams";
+    }
 }
