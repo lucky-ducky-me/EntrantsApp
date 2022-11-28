5af1b911ab3d03729b3b7f63ba216255a583f09b
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2022-11-28 16:04:09 +0300

    Изменение атрибутивной модели. Добавлен атрибут - кол-во баллов за экзамен.

--- src/main/java/vyatsu/fib/ovchinnikov/EntrantsApp/models/EntrantExamMap.java
+++ src/main/java/vyatsu/fib/ovchinnikov/EntrantsApp/models/EntrantExamMap.java
@@ -33,14 +33,30 @@ public class EntrantExamMap {
     private UUID examId;
 
     /**
+     * Количесто баллов за экзамен.
+     */
+    @Getter(AccessLevel.PUBLIC)
+    private Integer examScore;
+
+    /**
      * Создание связи с указанными параментарми.
      * @param id id связи.
      * @param entrant id абитуриента.
      * @param exam id экзамена.
+     * @param examScore количество баллоа за экзамен.
      */
-    public EntrantExamMap(UUID id, Entrant entrant, Exam exam) {
+    public EntrantExamMap(UUID id, Entrant entrant, Exam exam, Integer examScore) {
         setEntrantExamId(id);
         setEntrantId(entrant.getId());
         setExamId(exam.getId());
     }
+
+    public void setExamScore(Integer examScore) {
+        if (examScore < 0) {
+            throw new IllegalArgumentException("Количество баллов не может быть отрицательным.");
+        }
+
+        this.examScore = examScore;
+    }
+
 }
